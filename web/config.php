f52ff56ffb81382ca11dfba159f5580d3e1a7ea8
deepmean <deepmean@gmail.com>
2018-08-31 15:17:02 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
-define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
+define('GJ_CODE', 'TW');
+define('SITE_NAME', 'u2php');
+define('TITLENAME', 'Utube');
 define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
+define('EMAIL', 'deepmean@aliyun.com');
 ?>
